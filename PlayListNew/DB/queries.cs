9302ed7380c4c2aeec6d3efc351f12b9d0ba25a0
Shani <shani_z2007@walla.com>
2020-01-09 22:16:31 +0200

    start create playlist queries

--- PlayListNew/DB/queries.cs
+++ PlayListNew/DB/queries.cs
@@ -8,12 +8,99 @@ namespace PlayListNew.DB
 {
     class queries
     {
-        public static string creartPlaylistAllOptions = @" SELECT songs.id FROM playlistgame.songs
+        public static string creartPlaylistAllOptionsTest = @" SELECT songs.id FROM playlistgame.songs
                                         WHERE songs.tempo >= 0 AND songs.tempo <= 200
                                         AND songs.loudness >= -52 AND songs.loudness <= 1
                                         AND songs.hotness >= 0 and songs.hotness <=0.5
                                         AND (songs.year >= 1970 AND songs.year < 1990)
                                         AND (songs.duration >= 0 AND songs.duration < 300)
                                         LIMIT 200;";
+
+
+
+        public static string creartPlaylistAllOptionsSmall = @" SELECT songs.id FROM playlistgame.songs
+                            WHERE songs.tempo >= '{0} AND songs.tempo <= '{1}'
+                            AND songs.loudness >= '{2}' AND songs.loudness <= '{3}'
+                            AND songs.hotness >= 0 AND songs.hotness <= '{4}'
+                            AND (songs.year >= '{5}' AND songs.year < '{6} + 10')
+                            AND (songs.duration >= 0 AND songs.duration < '{7} + 60')
+                            LIMIT 5";
+
+
+        // insert playlist name from user to the playlists table, check the SELECT LAST_INSERT_ID and if ID
+        // automatically increased
+        public static string insertNewPlaylist = @"INSERT INTO playlistgame.playlists(playlists.playlist_name) VALUES ('{0}')";
+
+        // create playlist if user choose all options, and insert all songs choosed
+        // insert song id and playlist id to the songs_to_playlist table
+        public static string creartPlaylistAllOptions = @"INSERT INTO playlistgame.songs_to_playlist(songs_to_playlist.song_id, songs_to_playlist. playlist_id)
+                    VALUES (
+                    (SELECT playlists.playlist_id FROM playlistgame.playlists
+                    WHERE playlists.playlist_name = '{0}'),
+                    (SELECT songs.id FROM playlistgame.songs
+                    WHERE songs.tempo >= '{1} AND songs.tempo <= '{2}'
+                    AND songs.loudness >= '{3}' AND songs.loudness <= '{4}'
+                    AND songs.hotness >= 0 AND songs.hotness <= '{5}'
+                    AND (songs.year >= '{6}' AND songs.year < '{7} + 10')
+                    AND (songs.duration >= 0 AND songs.duration < '{8} + 60')
+                    LIMIT '{9}'))";
+
+        // when user choose i dont care pop and i dont care decade - check that numbers are ok!
+        public static string creartPlaylistWithoutPopAndDec = @"INSERT INTO playlistgame.songs_to_playlist(songs_to_playlist.song_id, songs_to_playlist. playlist_id)
+                    VALUES (
+                    (SELECT playlists.playlist_id FROM playlistgame.playlists
+                    WHERE playlists.playlist_name = '{0}'),
+                    (SELECT * FROM playlistgame.songs
+                    WHERE songs.tempo >= '{1} AND songs.tempo <= '{2}'
+                    AND songs.loudness >= '{3}' AND songs.loudness <= '{4}'
+                    AND (songs.duration >= 0 AND songs.duration < '{5} + 60')
+                    LIMIT '{6}')";
+
+        // when user choose only i dont care decade - check that numbers are ok!
+        public static string creartPlaylistWithoutDec = @"INSERT INTO playlistgame.songs_to_playlist(songs_to_playlist.song_id, songs_to_playlist. playlist_id)
+                    VALUES (
+                    (SELECT playlists.playlist_id FROM playlistgame.playlists
+                    WHERE playlists.playlist_name = '{0}'),
+                    (SELECT * FROM playlistgame.songs
+                    WHERE songs.tempo >= '{1} AND songs.tempo <= '{2}'
+                    AND songs.loudness >= '{3}' AND songs.loudness <= '{4}'
+                    AND songs.hotness >= 0 AND songs.hotness <= '{5}'
+                    AND (songs.duration >= 0 AND songs.duration < '{6} + 60')
+                    LIMIT '{7}'))";
+
+        // when user choose i dont care pop - check that numbers are ok!
+        public static string creartPlaylistWithoutPop = @"INSERT INTO playlistgame.songs_to_playlist(songs_to_playlist.song_id, songs_to_playlist. playlist_id)
+                    VALUES (
+                    (SELECT playlists.playlist_id FROM playlistgame.playlists
+                    WHERE playlists.playlist_name = '{0}'),
+                    (SELECT * FROM playlistgame.songs
+                    WHERE songs.tempo >= '{1} AND songs.tempo <= '{2}'
+                    AND songs.loudness >= '{3}' AND songs.loudness <= '{4}'
+                    AND (songs.year >= '{5}' AND songs.year < '{6} + 10')
+                    AND (songs.duration >= 0 AND songs.duration < '{7} + 60')
+                    LIMIT '{8}'))";
+
+        // show playlist songs - need to fix
+        public static string getPlaylistDetails = @"SELECT playlists.playlist_name, albums.album_name
+                    FROM songs
+                    INNER JOIN playlists ON playlists.song_id = songs.song_id
+                    INNER JOIN albums ON albums.album_id = songs.album_id";
+
+        // show all playlists of a specific user on the screen - sort it by date!
+        public static string getAllUserPlaylists = @"SELECT playlists.playlist_name
+                    FROM playlistgame.playlists, playlistgame.user_to_playlists
+                    WHERE user_id='{0}' AND playlists.playlist_id=user_to_playlists.playlist_id
+                    ORDER BY playlists.creation_date DESC";
+
+        // get user id when user logged in
+        public static string getUserIdAndName = @"SELECT users.user_id, users.full_name
+                    FROM playlistgame.users
+                    WHERE name = '{0}'";
+
     }
+
+
+
+
+
 }
--- PlayListNew/View/createPlaylist.xaml.cs
+++ PlayListNew/View/createPlaylist.xaml.cs
@@ -16,11 +16,27 @@ using PlayListNew.DB;
 
 namespace PlayListNew.View
 {
+    
+    
     /// <summary>
     /// Interaction logic for createPlaylist.xaml
     /// </summary>
     public partial class createPlaylist : Window
     {
+        private double popularity;
+        private double minTempoRange;
+        private double maxTempoRange;
+        private double minLoudnessRange;
+        private double maxLoudnessRange;
+        private int minDecadeRange;
+        private int maxDecadeRange;
+        private int duration;
+        private int numOfSongs;
+        private string playlistName;
+        private bool isDontCareDecChoosed;
+        private int numOfDecChoosed = 0;
+        private List<int> decadesRanges = new List<int>();
+
         public createPlaylist()
         {
             InitializeComponent();
@@ -29,8 +45,62 @@ namespace PlayListNew.View
         public void pressCreate(object sender, RoutedEventArgs e)
         {
 
+            // get all values inserted by the user
+            this.popularity = (double)popularitySlider.Value;
+            this.minTempoRange = double.Parse(tempoMin.Text);
+            this.maxTempoRange = double.Parse(tempoMax.Text);
+            this.minLoudnessRange = double.Parse(loudnessMin.Text);
+            this.maxLoudnessRange = double.Parse(loudnessMax.Text);
+            this.duration = (int)durationSlider.Value;
+            this.numOfSongs = int.Parse(songsNum.Text);
+            this.playlistName = playListName.Text;
+
+
+            // checks hoe many checkboxes of decades user choosed
+            if (this.isDontCareDecChoosed)
+            {
+
+            }
+
             DataBaseHandler dbhandler = DataBaseHandler.Instance;
             dbhandler.createPlaylist();
+        }
+
+
+        private void choose70(object sender, RoutedEventArgs e)
+        {
+            this.decadesRanges.Add(1970);
+            this.decadesRanges.Add(1979);
+            this.numOfDecChoosed++;
+
+        }
+
+        private void choose80(object sender, RoutedEventArgs e)
+        {
+            this.decadesRanges.Add(1980);
+            this.decadesRanges.Add(1989);
+            this.numOfDecChoosed++;
+            
+        }
+
+        private void choose90(object sender, RoutedEventArgs e)
+        {
+            this.decadesRanges.Add(1990);
+            this.decadesRanges.Add(1999);
+            this.numOfDecChoosed++;
+        }
+
+        private void choose00(object sender, RoutedEventArgs e)
+        {
+            this.decadesRanges.Add(2000);
+            this.decadesRanges.Add(2009);
+            this.numOfDecChoosed++;
+        }
+
+        private void chooseAllDec(object sender, RoutedEventArgs e)
+        {
+            this.isDontCareDecChoosed = true;
+            numOfDecChoosed = 0;
         }
 
     }
